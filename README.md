b50196a7576bdb2d9cee1a765cf7a64739fd52a6
Aaron Wong <aaronwong13@gmail.com>
2016-12-22 14:02:47 -0800

    exports.function_identifier vs module.exports

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ A simple Node project built using the Express framework and crypto for hashing.
 
 ## Setup
 ```bash
+// in project directory
 $ npm install
+```
 
+## Usage
+```bash
 $ node app.js
 ```
--- urly.js
+++ urly.js
@@ -1,13 +1,9 @@
 const crypto = require('crypto')
 
-function shorten(url){
+exports.shorten = url => {
   let short_id = crypto.createHmac('sha256', url)
                    	 .update('Super Secrit')
                    	 .digest('hex')
 										 .substring(0,5)
   return short_id
-}
-
-module.exports = {
-  shorten
 }
