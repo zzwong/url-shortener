755a5e35572c27af65adac399716f0115a8e9958
Aaron Wong <aaronwong13@gmail.com>
2017-07-07 00:26:45 -0700

    1 line refactor

--- urly.js
+++ urly.js
@@ -1,9 +1,3 @@
 const crypto = require('crypto')
 
-exports.shorten = url => {
-  let short_id = crypto.createHmac('sha256', url)
-                   	 .update('Super Secrit')
-                   	 .digest('hex')
-										 .substring(0,5)
-  return short_id
-}
+exports.shorten = url => crypto.createHmac('sha256', url).update('Super Secrit').digest('hex').substring(0,5);
